eca59c2a7f77b030ddd5a0f6d8611785190cb696
Gaëtan <gaetan.rival1@etu.univ-grenoble-alpes.fr>
2020-01-17 10:48:17 +0100

    Main v1

--- TP1/baby_step/src/baby_step/Main.java
+++ TP1/baby_step/src/baby_step/Main.java
@@ -1,5 +1,14 @@
 package baby_step;
 
 public class Main {
+	Client client;
+	Server server;
 
+	public static void main(String[] args) {
+		server= new Server(11799);
+		
+		server.start();
+		client=new Client(test,server,11799);
+		
+	}
 }
