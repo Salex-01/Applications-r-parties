524d2691814293b77659693ae970165e6a860d2a
Salex <salex@orange.fr>
2020-03-05 22:39:03 +0100

    Step2 ok

--- TP2/ricm.channels/src/ricm/channels/nio/Broker.java
+++ TP2/ricm.channels/src/ricm/channels/nio/Broker.java
@@ -40,39 +40,43 @@ public class Broker implements ricm.channels.IBroker {
 	}
 
 	@Override
-	public boolean connect(String host, int port) throws IOException {
-		// create a new non-blocking server socket channel
-		sc = SocketChannel.open();
-		sc.configureBlocking(false);
+	public boolean connect(String host, int port) {
+		try {
+			// create a new non-blocking server socket channel
+			sc = SocketChannel.open();
+			sc.configureBlocking(false);
 
-		// register an connect interested in order to get a
-		// connect event, when the connection will be established
-		scKey = sc.register(selector, SelectionKey.OP_CONNECT);
+			// register an connect interested in order to get a
+			// connect event, when the connection will be established
+			scKey = sc.register(selector, SelectionKey.OP_CONNECT);
 
-		// request a connection to the given server and port
-		InetAddress addr = InetAddress.getByName(host);
-		sc.connect(new InetSocketAddress(addr, port));
-		return true;
+			// request a connection to the given server and port
+			InetAddress addr = InetAddress.getByName(host);
+			sc.connect(new InetSocketAddress(addr, port));
+			return true;
+		} catch (IOException e) {
+			IBL.refused(host, port);
+			return false;
+		}
 	}
 
 	@Override
-	public boolean accept(int port) throws IOException {
-		// create a new non-blocking server socket channel
-		ssc = ServerSocketChannel.open();
-		ssc.configureBlocking(false);
+	public boolean accept(int port) {
+		try {
+			// create a new non-blocking server socket channel
+			ssc = ServerSocketChannel.open();
+			ssc.configureBlocking(false);
 
-		// bind the server socket to the given address and port
-		InetAddress hostAddress;
-		hostAddress = InetAddress.getByName("localhost");
-		InetSocketAddress isa = new InetSocketAddress(hostAddress, port);
-		ssc.socket().bind(isa);
+			// bind the server socket to the given address and port
+			InetSocketAddress isa = new InetSocketAddress(port);
+			ssc.socket().bind(isa);
 
-		// be notified when connection requests arrive
-		sscKey = ssc.register(selector, SelectionKey.OP_ACCEPT);
-
-		////////////////// Je sais pas comment retourner un boolean dans ce cas
-		////////////////// ????\\\\\\\\\\\\\\\\\\\\\\\\
-		return ((sscKey.interestOps() & SelectionKey.OP_ACCEPT) != 0);
+			// be notified when connection requests arrive
+			sscKey = ssc.register(selector, SelectionKey.OP_ACCEPT);
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
 	}
 
 	public void run() throws IOException {
@@ -83,25 +87,25 @@ public class Broker implements ricm.channels.IBroker {
 			while (selectedKeys.hasNext()) {
 				SelectionKey key = (SelectionKey) selectedKeys.next();
 				// process key's events
-				if (key.isValid() && key.isAcceptable()) {
-					SelectableChannel sc1 = ssc.accept();
-					Channel c = new Channel(sc1, this);
-					channels.put(sc1, c); // Channel associated with its own SelectableChannel
-					IBL.accepted(c);
-					key.interestOps(SelectionKey.OP_WRITE | SelectionKey.OP_READ);
-				}
-				if (key.isValid() && key.isReadable()) {
-					channels.get(key.channel()).handleRead(key);
-				}
-				if (key.isValid() && key.isWritable()) {
-					channels.get(key.channel()).handleWrite(key);
-				}
-				if (key.isValid() && key.isConnectable()) {
-					sc.finishConnect();
-					Channel c = new Channel(sc, this);
-					channels.put(sc, c);
-					IBL.connected(c);
-					key.interestOps(SelectionKey.OP_WRITE | SelectionKey.OP_READ);
+				if (key.isValid()) {
+					if (key.isAcceptable()) {
+						SelectableChannel sc1 = ssc.accept();
+						sc1.configureBlocking(false);
+						scKey = sc1.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+						Channel c = new Channel(sc1, this);
+						channels.put(sc1, c); // Channel associated with its own SelectableChannel
+						IBL.accepted(c);
+					} else if (key.isReadable()) {
+						channels.get(key.channel()).handleRead(key);
+					} else if (key.isWritable()) {
+						channels.get(key.channel()).handleWrite(key);
+					} else if (key.isConnectable()) {
+						sc.finishConnect();
+						key.interestOps(SelectionKey.OP_WRITE | SelectionKey.OP_READ);
+						Channel c = new Channel(sc, this);
+						channels.put(sc, c);
+						IBL.connected(c);
+					}
 				}
 				// remove the key from the selected-key set
 				selectedKeys.remove();
