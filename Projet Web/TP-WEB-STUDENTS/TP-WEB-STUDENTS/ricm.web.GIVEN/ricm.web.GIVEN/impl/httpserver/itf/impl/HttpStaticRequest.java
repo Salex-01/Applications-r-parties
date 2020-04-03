7fe64d26a14307ac78003c0b801e9afadba6c518
Gaëtan <gaetan.rival1@etu.univ-grenoble-alpes.fr>
2020-04-03 08:30:47 +0200

    Projet Web

---
+++ Projet Web/TP-WEB-STUDENTS/TP-WEB-STUDENTS/ricm.web.GIVEN/ricm.web.GIVEN/impl/httpserver/itf/impl/HttpStaticRequest.java
@@ -0,0 +1,49 @@
+package httpserver.itf.impl;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import httpserver.itf.HttpRequest;
+import httpserver.itf.HttpResponse;
+
+public class HttpStaticRequest extends HttpRequest {
+	static final int BUF_SZ = 1024;
+	static final String DEFAULT_FILE = "index.html";
+	
+	public HttpStaticRequest(HttpServer hs, String method, String ressname) throws IOException {
+		super(hs, method, ressname);
+	}
+	
+	public void process(HttpResponse resp) throws Exception {
+		
+		String folder=m_hs.getFolder().getName();	//Get name of the file
+		m_ressname=folder+m_ressname;
+		File file= new File(m_ressname);	//Create a file with the same URL
+		
+		if(file.exists()) {
+			
+		
+			
+			resp.setReplyOk();
+			resp.setContentLength((int)file.length());
+			resp.setContentType(getContentType(m_ressname));
+			
+			PrintStream ps = resp.beginBody();
+			FileInputStream flux = new FileInputStream(file);
+			
+			ps.print(flux.readAllBytes());
+			flux.close();
+			
+			ps.flush();
+			ps.close();
+			
+		}
+		else {
+			resp.setReplyError(404, "File not Found");
+		}
+	}
+
+}
